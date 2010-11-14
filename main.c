6ab073279d4f0c509a9e3a59cf605d13c85adef5
Martin Venuš <martin.venus@gmail.com>
2010-11-14 18:39:51 +0100

    Pridana podpora paralelizace programu

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
-/* 
+/*
  * File:   main.c
  * Author: Martin Venuš, Jaroslav Líbal
  *
  * Created on 11. říjen 2010, 14:53
  */
 
+#include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,24 +23,77 @@
  */
 int main(int argc, char** argv) {
 
-    char inputFile[255] = "";
+    /*
+     * Inicializace a nastavení knihovny MPI
+     */
+    int my_rank;
+    int p;
+    MPI_Status status;
 
-    int i = 0;
-    int j = 0;
+    /* start up MPI */
+    MPI_Init(&argc, &argv);
 
-    //Načtení proměnných z příkazového řádku
-    while (argv[i] != NULL) {
-        //printf("Argument: %s\n", argv[i]);
-        if (strcmp(argv[i], "-f") == 0) {
-            strcpy(inputFile, argv[i + 1]);
-        }
-        i++;
-    }
+    /* find out process rank */
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    /* find out number of processes */
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    /*
+     * Inicializace dokončena
+     */
 
     int** maticeSousednosti;
-    maticeSousednosti = loadFile(inputFile);
+    int pocetVrcholu;
 
-    int pocetVrcholu = getPocetVrcholu();
+
+    if (my_rank == 0) {
+
+        char inputFile[255] = "";
+
+        int i = 0;
+        int j = 0;
+
+        //Načtení proměnných z příkazového řádku
+        while (argv[i] != NULL) {
+            //printf("Argument: %s\n", argv[i]);
+            if (strcmp(argv[i], "-f") == 0) {
+                strcpy(inputFile, argv[i + 1]);
+            }
+            i++;
+        }
+
+        maticeSousednosti = loadFile(inputFile);
+
+        pocetVrcholu = getPocetVrcholu();
+
+        int k = 0;
+        for (k = 1; k < p; k++) {
+            int l = 0;
+            for (l = 0; l < pocetVrcholu; l++) {
+                MPI_Send(maticeSousednosti[l], pocetVrcholu, MPI_INT, k, 0, MPI_COMM_WORLD);
+            }
+        }
+    }
+
+    MPI_Bcast(&pocetVrcholu, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (my_rank > 0) {
+        int matrix[pocetVrcholu][pocetVrcholu];
+
+        printf("procesor %d přijímá\n", my_rank);
+
+        int l = 0;
+        for (l = 0; l < pocetVrcholu; l++) {
+            MPI_Recv(&matrix[l], pocetVrcholu, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        }
+
+        printf("Matice(1): %d\n", matrix[pocetVrcholu-1][pocetVrcholu-1-1]);
+
+    }
+
+    //printf("Počet vrcholů: %i\n", pocetVrcholu);
+
+    /*
 
     Stack s;
 
@@ -52,7 +106,7 @@ int main(int argc, char** argv) {
 
     //Uvolníme paměť alokovanou pro matici
     //free(DFS);
-    
+
     findBestColouring();
     setBestSolutionToMatrix(pocetVrcholu);
 
@@ -62,6 +116,10 @@ int main(int argc, char** argv) {
 
     memoryFreeStack(&s);
     memoryFreeArray();
+     */
+
+    /* shut down MPI */
+    MPI_Finalize();
 
     return (EXIT_SUCCESS);
 }
